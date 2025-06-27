e948dac97599b823781b555d96790b4468ead5e5
Balaj Marius <balajmarius@gmail.com>
2025-06-27 13:49:52 +0300

    feat: added Link component

---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,21 @@
+import { ReactNode, HTMLAttributes } from "react";
+
+import { Typography } from "@/components/Typography";
+import { SvgIconArrow } from "@/components/SvgIcon";
+
+export type LinkProps = {
+  href: string;
+  children: ReactNode;
+  target?: "_blank" | "_self" | "_parent" | "_top";
+} & HTMLAttributes<HTMLAnchorElement>;
+
+const Link = ({ children, href, target = "_self", ...props }: LinkProps) => {
+  return (
+    <a href={href} target={target} className="flex items-center gap-1" {...props}>
+      <Typography variant="body1">{children}</Typography>
+      <SvgIconArrow size="small" className="text-blue-500" />
+    </a>
+  );
+};
+
+export default Link;
