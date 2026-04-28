d7fbc80341c8d28e8f3aeaca1c81407eb94649a5
Balaj Marius <balajmarius@gmail.com>
2026-04-28 15:39:45 +0300

    refactor: simplify video bookmark card

--- src/components/folders-list/folders-list-card-video.tsx
+++ src/components/folders-list/folders-list-card-video.tsx
@@ -2,11 +2,7 @@ import { useTranslations } from "next-intl";
 
 import { Link } from "@/components/link";
 import { Typography } from "@/components/typography";
-import {
-  SvgIconMiddleCap,
-  SvgIconTopCap,
-  SvgIconVideo,
-} from "@/components/svg-icon";
+import { SvgIconVideo } from "@/components/svg-icon";
 
 type FoldersListCardVideoProps = {
   title: string;
@@ -18,28 +14,22 @@ const FoldersListCardVideo = ({ title, url }: FoldersListCardVideoProps) => {
   const t = useTranslations();
 
   return (
-    <div className="overflow-hidden drop-shadow-2xl">
-      <SvgIconTopCap size="inherit" className="w-full text-gray-300" />
+    <div className="drop-shadow-2xl">
+      <div className="bg-gray-300">
+        <div className="flex flex-col gap-3 px-6 py-3">
+          <div className="flex items-center justify-center bg-blue-500 rounded-sm size-6">
+            <SvgIconVideo size="small" className="text-gray-300" />
+          </div>
 
-      <div className="flex flex-col gap-3 px-6 py-3 bg-gray-300">
-        <div className="size-6 bg-blue-500 rounded-sm flex items-center justify-center">
-          <SvgIconVideo size="small" className="text-gray-300" />
+          <Typography variant="subtitle1">{title}</Typography>
         </div>
-        <Typography variant="subtitle1">{title}</Typography>
+
+        <div className="px-6 py-1">
+          <Link href={url} target="_blank">
+            {t("bookmarks.watchTheVideo")}
+          </Link>
+        </div>
       </div>
-
-      <SvgIconMiddleCap size="inherit" className="w-full text-gray-300" />
-
-      <div className="px-6 py-1 bg-gray-300">
-        <Link href={url} target="_blank">
-          {t("bookmarks.watchTheVideo")}
-        </Link>
-      </div>
-
-      <SvgIconTopCap
-        size="inherit"
-        className="w-full text-gray-300 rotate-180"
-      />
     </div>
   );
 };
